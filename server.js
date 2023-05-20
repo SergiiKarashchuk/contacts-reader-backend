2ca69d865d2bb27288bc9da1e84bca48e87030c6
Sergii <kspost01@gmail.com>
2023-05-20 23:24:10 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+const mongoose = require("mongoose");
+
+const DB_HOST = "mongodb+srv://Sergii:M4Kg2TQ1VUakIOtL@cluster0.4i8grq6.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+
+mongoose.set("strictQuery", true);
+mongoose.connect(DB_HOST)
+.then (() => {app.listen(3000)})
+.catch ( error => {console.log(error.message),
+process.exit(1)})
+
 const app = require('./app')
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
