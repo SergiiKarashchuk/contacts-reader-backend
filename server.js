c66909677ea01b9a3e7b290e7746e9facce67a14
Sergii <kspost01@gmail.com>
2023-05-21 10:49:15 +0300

    add dotenv

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const DB_HOST = "mongodb+srv://Sergii:M4Kg2TQ1VUakIOtL@cluster0.4i8grq6.mongodb.net/contacts_reader?retryWrites=true&w=majority"
+const app = require('./app')
+
+const { DB_HOST, PORT=3000 } = process.env;
 
 mongoose.set("strictQuery", true);
+
 mongoose.connect(DB_HOST)
-.then (() => {app.listen(3000)})
+.then (() => {app.listen(PORT)})
 .catch ( error => {console.log(error.message),
 process.exit(1)})
 
-const app = require('./app')
+
 
